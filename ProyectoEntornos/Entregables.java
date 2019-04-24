71f8cc9719b04756eff11e1b8df5f3ed8a76a72a
JavierPerezN <49001120+JavierPerezN@users.noreply.github.com>
2019-04-24 19:43:52 +0200

    Add files via upload

---
+++ ProyectoEntornos/Entregables.java
@@ -0,0 +1,8 @@
+package prueba;
+
+public interface Entregables {
+	public void entregar();
+	public void devolver();
+	public void idEntregado();
+	public void compareTo();
+}
---
+++ ProyectoEntornos/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+
+	public static void main(String[] args) {
+		Series[] s=new Series[];
+		Videojuegos[] v=new Videojuegos[];
+		añadirSerie(s);
+	}
+
+}
